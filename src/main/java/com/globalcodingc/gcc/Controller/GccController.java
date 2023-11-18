dca6064f9b713de4e5ce7db68831d4279a450f5b
Balkrishna <balaharode@gmail.com>
2023-11-18 17:04:21 +0530

    p2

--- src/main/java/com/globalcodingc/gcc/Controller/GccController.java
+++ src/main/java/com/globalcodingc/gcc/Controller/GccController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.globalcodingc.gcc.Service.FileReorg;
 import com.globalcodingc.gcc.Service.ProfitMaximization;
 import org.json.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,9 @@ public class GccController {
     @Autowired
     ProfitMaximization profitMaximization;
 
+    @Autowired
+    FileReorg fileReorg;
+
 
     @PostMapping(value = "/profit-maximization", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> profitMaximization(@RequestBody String json) throws JsonProcessingException {
@@ -41,6 +45,25 @@ public class GccController {
                 .replaceAll("\\]", "")
                 .split(" ,"))};
         List<Integer> answer = profitMaximization.findMaxProfit(clear);
+
+        Map<String, Object> ans = new HashMap<>();
+        ans.put("answer", answer);
+        return ResponseEntity.ok(objectMapper.writeValueAsString(ans));
+    }
+
+
+    @PostMapping(value = "/file-reorganization", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> fileReorg(@RequestBody String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map
+                = objectMapper.readValue(json, new TypeReference<Map<String, Object>>() {
+        });
+
+        String input = map.values().toString();
+        String[] clear = new String[]{Arrays.toString(input.replaceAll("\\[", "")
+                .replaceAll("\\]", "")
+                .split(" ,"))};
+        List<Integer> answer = fileReorg.findpalindrome(clear);
 
         Map<String, Object> ans = new HashMap<>();
         ans.put("answer", answer);
---
+++ src/main/java/com/globalcodingc/gcc/Service/FileReorg.java
@@ -0,0 +1,43 @@
+package com.globalcodingc.gcc.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FileReorg {
+    public int longestPalindrome(String s) {
+
+        int[] count = new int[128];
+        for (char c : s.toCharArray())
+            count[c]++;
+
+        int oddCount = -1;
+        for (int v : count) {
+            if (v % 2 != 0) oddCount++;
+        }
+
+        return (oddCount > 0) ? s.length() - oddCount : s.length();
+    }
+
+
+    public List<Integer> findpalindrome(String[] payload) {
+
+        List<Integer> answer = new ArrayList<>();
+
+        String[] input = payload[0].split(", ");
+
+        for (int k = 0; k < input.length; k++) {
+
+                answer.add(longestPalindrome(input[k]));
+
+        }
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println(answer.get(i));
+        }
+
+        return answer;
+
+    }
+}
